e873aabb9b40cba7e15c778ac1d417239db97752
Moise MM <moisemutangala39@gmail.com>
2022-02-24 00:37:25 +0200

    traitement du formulaire pour enregistrer les informations dans la BDD grace à la methode new se trouvant dans ClientController

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,13 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Form\ClientType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
-    #[Route('/', name: 'client')]
+
+    //constructeur
+    public function __construct(private EntityManagerInterface $em){}
+
+    #[Route('/', name: 'client.index')]
     public function index(): Response
     {
         return $this->render('client/index.html.twig', [
@@ -20,13 +26,27 @@ class ClientController extends AbstractController
 
 
     #[Route('/client/new', name: 'client.create')]
-    public function create(): Response
+    public function create(Request $request): Response
     {
         //creation de l'objet Client
         $client = new Client();
 
         //creation du formulaire
         $form = $this->createForm(ClientType::class,$client);
+
+        //analyse la requete
+        $form->handleRequest($request);
+
+        //verifie si le formulaire est envoyé et qu'il est valide
+        if($form->isSubmitted() && $form->isValid())
+        {
+            //
+            $this->em->persist($client);
+            $this->em->flush();
+
+            //redirection
+            return $this->redirectToRoute('client.index');
+        }
 
         return $this->render('client/new.html.twig',[
             'form' => $form->createView()
